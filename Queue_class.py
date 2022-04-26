bf0911e068ad722f2607fde937f9d7df8034443f
kareemadesola <kareemadesola1999@gmail.com>
2022-04-26 20:58:15 +0100

    Add is_empty method

--- Queue_class.py
+++ Queue_class.py
@@ -13,17 +13,20 @@ class Queue:
         return self.storage.append(new_element)
 
     def peek(self):
-        return self.storage[0]
+        return self.storage[0] if len(self.storage) else -1
 
     def dequeue(self):
-        return self.storage.pop(0)
+        return self.storage.pop(0) if len(self.storage) else -1
+
+    def is_empty(self):
+        return True if len(self.storage) else False
 
 
 if __name__ == '__main__':
     # setup
     q = Queue(1)
-    q.enqueue(2)
-    q.enqueue(3)
+    # q.enqueue(2)
+    # q.enqueue(3)
 
     # Test peek
     # Should be 1
@@ -34,6 +37,7 @@ if __name__ == '__main__':
     # Should be 2
 
     print(q.dequeue())
-    q.enqueue(5)
+    # q.enqueue(5)
     # Should be 5
     print(q.peek())
+    print(q.is_empty())

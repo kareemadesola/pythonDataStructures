c88877e8b3d054e407cce7a231339b538dc7f5b0
Kareem Adesola <kareemadesola1999@gmail.com>
2022-04-30 15:40:33 +0100

    Wrote tests for class

--- CrackingTheCodingInterview/ch02/linked_list.py
+++ CrackingTheCodingInterview/ch02/linked_list.py
@@ -1,44 +1,94 @@
-from typing import Union, List
+import random
+import unittest
+from copy import copy
+from typing import Union, List, Optional
 
 
 class LinkedListNode:
     def __init__(self, value: Union[int, str], next_node=None, prev_node=None):
-        pass
+        self.value = value
+        self.next: Optional[LinkedListNode] = next_node
+        self.prev: Optional[LinkedListNode] = prev_node
 
     def __str__(self):
-        pass
+        return str(self.value)
 
 
 class LinkedList:
     def __init__(self, values=None):
-        pass
+        self.head: Optional[LinkedListNode] = None
+        self.tail: Optional[LinkedListNode] = None
+        if values:
+            self.add_multiple(values)
 
     def __iter__(self):
-        pass
+        current_node = self.head
+        while current_node is not None:
+            yield current_node
+            current_node = current_node.next
 
     def __str__(self):
-        pass
+        return " -> ".join(str(i) for i in self)
 
     def __len__(self):
-        pass
+        length = 0
+        for _ in self:
+            length += 1
+        return length
 
     def values(self):
-        pass
+        return [i.value for i in self]
 
     def add(self, value: Union[int, str]):
-        pass
+        if self.head is None:
+            self.head = self.tail = LinkedListNode(value)
+        else:
+            self.tail.next = LinkedListNode(value)
+            self.tail = self.tail.next
+        return self.tail
 
     def add_to_beginning(self, value: Union[int, str]):
-        pass
+        if not self.head:
+            self.head = self.tail = LinkedListNode(value)
+        else:
+            self.head = LinkedListNode(value, self.head)
+        return self.head
 
     def add_multiple(self, values: List[Union[int, str]]):
-        pass
+        for v in values:
+            self.add(v)
 
     @classmethod
     def generate(cls, k: int, min_value: int, max_value: int):
-        pass
+        return cls(random.choices(range(min_value, max_value), k=k))
 
 
 class DoublyLinkedList(LinkedList):
     def add(self, value: Union[int, str]):
-        pass
+        if not self.head:
+            self.head = self.tail = LinkedListNode(value)
+        else:
+            self.tail.next = LinkedListNode(value, None, self.tail)
+            self.tail = self.tail.next
+        return self.tail
+
+
+class Test(unittest.TestCase):
+    ll = LinkedList([1, 2, 3, 4, 5])
+
+    def test_add_to_beginning(self):
+        ll = copy(self.ll)
+        ll.add_to_beginning(5)
+        self.assertTrue(ll.values(), [5, 1, 2, 3, 4, 5])
+
+    def test_value(self):
+        self.assertTrue(self.ll.values(), [1, 2, 3, 4])
+
+    def test_len(self):
+        self.assertTrue(len(self.ll), 5)
+
+    def test_str(self):
+        self.assertTrue(self.ll, "1 -> 2 -> 3 -> 4 -> 5")
+
+    def test_generate(self):
+        self.assertTrue(len(LinkedList.generate(10, 2, 8)), 10)

cfbeb238b0de47dc29a343cd030e06d7c21b1bfd
kareemadesola <kareemadesola1999@gmail.com>
2022-05-01 13:23:16 +0100

    make node and linkedlist classes reusable

--- CrackingTheCodingInterview/ch02/linked_list.py
+++ CrackingTheCodingInterview/ch02/linked_list.py
@@ -4,22 +4,77 @@ from copy import copy
 from typing import Union, List, Optional
 
 
-class LinkedListNode:
-    def __init__(self, value: Union[int, str], next_node=None, prev_node=None):
+class SinglyLinkedListNode:
+    def __init__(self, value: Union[int, str], next_node=None):
         self.value = value
-        self.next: Optional[LinkedListNode] = next_node
-        self.prev: Optional[LinkedListNode] = prev_node
+        self.next: Optional[SinglyLinkedListNode] = next_node
 
     def __str__(self):
         return str(self.value)
 
 
+class DoublyLinkedListNode(SinglyLinkedListNode):
+    def __init__(self, value: Union[int, str], next_node=None, prev_node=None):
+        super().__init__(value, next_node)
+        self.prev: Optional[DoublyLinkedListNode] = prev_node
+
+
+class LinkedListMeta(type):
+    """Linked List Metaclass"""
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+
+    def __subclasscheck__(cls, subclass):
+        return hasattr(subclass, '__iter__') and \
+               callable(subclass.__iter__) and \
+               hasattr(subclass, '__str__') and \
+               callable(subclass.__str__) and \
+               hasattr(subclass, '__len__') and \
+               callable(subclass.__len__) and \
+               hasattr(subclass, 'values') and \
+               callable(subclass.values) and \
+               hasattr(subclass, 'add') and \
+               callable(subclass.add) and \
+               hasattr(subclass, 'add_to_beginning') and \
+               callable(subclass.add_to_beginning) and \
+               hasattr(subclass, 'add_multiple') and \
+               callable(subclass.add_multiple)
+
+
 class LinkedList:
     def __init__(self, values=None):
-        self.head: Optional[LinkedListNode] = None
-        self.tail: Optional[LinkedListNode] = None
+        self.head: Optional[SinglyLinkedListNode] = None
+        self.tail: Optional[SinglyLinkedListNode] = None
         if values:
             self.add_multiple(values)
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def values(self):
+        pass
+
+    def add(self, value: Union[int, str]):
+        pass
+
+    def add_to_beginning(self, value: Union[int, str]):
+        pass
+
+    def add_multiple(self, values: List[Union[int, str]]):
+        pass
+
+    @classmethod
+    def generate(cls, k: int, min_value: int, max_value: int):
+        return cls(random.choices(range(min_value, max_value), k=k))
+
+
+class SinglyLinkedList(LinkedList):
 
     def __iter__(self):
         current_node = self.head
@@ -41,17 +96,17 @@ class LinkedList:
 
     def add(self, value: Union[int, str]):
         if self.head is None:
-            self.head = self.tail = LinkedListNode(value)
+            self.head = self.tail = SinglyLinkedListNode(value)
         else:
-            self.tail.next = LinkedListNode(value)
+            self.tail.next = SinglyLinkedListNode(value)
             self.tail = self.tail.next
         return self.tail
 
     def add_to_beginning(self, value: Union[int, str]):
         if not self.head:
-            self.head = self.tail = LinkedListNode(value)
+            self.head = self.tail = SinglyLinkedListNode(value)
         else:
-            self.head = LinkedListNode(value, self.head)
+            self.head = SinglyLinkedListNode(value, self.head)
         return self.head
 
     def add_multiple(self, values: List[Union[int, str]]):
@@ -63,18 +118,21 @@ class LinkedList:
         return cls(random.choices(range(min_value, max_value), k=k))
 
 
-class DoublyLinkedList(LinkedList):
+class DoublyLinkedList(SinglyLinkedList):
+    def __str__(self):
+        return " <--->>> ".join(str(i) for i in self)
+
     def add(self, value: Union[int, str]):
         if not self.head:
-            self.head = self.tail = LinkedListNode(value)
+            self.head = self.tail = DoublyLinkedListNode(value)
         else:
-            self.tail.next = LinkedListNode(value, None, self.tail)
+            self.tail.next = DoublyLinkedListNode(value, None, self.tail)
             self.tail = self.tail.next
         return self.tail
 
 
 class Test(unittest.TestCase):
-    ll = LinkedList([1, 2, 3, 4, 5])
+    ll = SinglyLinkedList([1, 2, 3, 4, 5])
 
     def test_singly_add(self):
         ll = copy(self.ll)
@@ -87,6 +145,7 @@ class Test(unittest.TestCase):
         ll.add(17)
         self.assertTrue(ll.values(), [1, 2, 3, 10, 17])
         print(ll)
+        print(ll.head)
 
     def test_add_to_beginning(self):
         ll = copy(self.ll)
@@ -104,4 +163,4 @@ class Test(unittest.TestCase):
         self.assertTrue(self.ll, "1 -> 2 -> 3 -> 4 -> 5")
 
     def test_generate(self):
-        self.assertTrue(len(LinkedList.generate(10, 2, 8)), 10)
+        self.assertTrue(len(SinglyLinkedList.generate(10, 2, 8)), 10)

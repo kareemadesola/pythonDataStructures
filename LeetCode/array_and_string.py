4fb4af517cfb0e1093d249fb84b7db30b70587af
kareemadesola <kareemadesola1999@gmail.com>
2022-05-13 10:55:46 +0100

    solve dominant index using pointers and hint

--- LeetCode/array_and_string.py
+++ LeetCode/array_and_string.py
@@ -12,6 +12,33 @@ def pivot_index(nums: List[int]) -> int:
     return -1
 
 
+def dominant_index(nums: List[int]) -> int:
+    max_index = 0
+    largest = 0
+    second_largest = 0
+    for index, value in enumerate(nums):
+        if value > largest:
+            second_largest = largest
+            largest, max_index = value, index
+        elif value > second_largest:
+            second_largest = value
+    if largest >= 2 * second_largest:
+        return max_index
+    return -1
+
+
+def dominant_index_hint(nums: List[int]) -> int:
+    max_index = 0
+    largest = 0
+    for index, value in enumerate(nums):
+        if value > largest:
+            largest, max_index = value, index
+    for index, value in nums:
+        if 2 * value > largest and index != max_index:
+            return -1
+    return max_index
+
+
 class Test(unittest.TestCase):
     def test_pivot_index(self):
         nums = [-1, -1, -1, -1, -1, 0]

6024394782f9c931daf3def302e6ac1211eca20a
kareemadesola <kareemadesola1999@gmail.com>
2022-06-01 07:36:24 +0100

    running sum using sum variable or input array for output

--- LeetCode/daily.py
+++ LeetCode/daily.py
@@ -2,6 +2,7 @@ import math
 
 # time and space 0(1)
 import unittest
+from typing import List
 
 
 def divide(dividend: int, divisor: int) -> int:
@@ -36,7 +37,36 @@ def divide_from_zero(dividend: int, divisor: int) -> int:
     return res if dividend ^ divisor > 0 else -res
 
 
+def running_sum_enumerate(nums: List[int]) -> List[int]:
+    sum_ = 0
+    for index, value in enumerate(nums):
+        nums[index] += sum_
+        sum_ += value
+    return nums
+
+
+# time 0(n)
+# space O(1)
+def running_sum(nums: List[int]) -> List[int]:
+    sum_ = 0
+    for index in range(len(nums)):
+        nums[index], sum_ = sum_ + nums[index], sum_ + nums[index]
+    return nums
+
+
+def running_sum_better(nums: List[int]) -> List[int]:
+    for index in range(1, len(nums)):
+        nums[index] += nums[index - 1]
+    return nums
+
+
 class Test(unittest.TestCase):
+    def test_running_sum_better(self):
+        self.assertEqual([1, 3, 6, 10], running_sum_better([1, 2, 3, 4]))
+
+    def test_running_sum_enumerate(self):
+        self.assertEqual([1, 3, 6, 10], running_sum_enumerate([1, 2, 3, 4]))
+
     def test_divide_from_zero(self):
         self.assertEqual(5, divide_from_zero(15, 3))
 

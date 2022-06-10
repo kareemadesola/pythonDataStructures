1c067b11988158098634baf9b43fbbfb01e3e0e0
kareemadesola <kareemadesola1999@gmail.com>
2022-06-10 06:46:15 +0100

    update linked list design implementation using len and a size watch

--- LeetCode/linked_list_design.py
+++ LeetCode/linked_list_design.py
@@ -8,6 +8,7 @@ class MyLinkedList:
 
     def __init__(self, head=None):
         self.head: Node = head
+        self.size = len(self) if self.head else 0
 
     def __len__(self):
         count = 0
@@ -18,7 +19,7 @@ class MyLinkedList:
         return count
 
     def get(self, index: int) -> int:
-        if not 0 <= index < len(self):
+        if not 0 <= index < self.size:
             return -1
         ll = self.head
         for _ in range(index):
@@ -27,18 +28,23 @@ class MyLinkedList:
 
     def addAtHead(self, val: int) -> None:
         self.head = Node(val, self.head)
+        self._inc_size()
+
+    def _inc_size(self):
+        self.size += 1
 
     def addAtTail(self, val: int) -> None:
         if not self.head:
             self.addAtHead(val)
             return
         ll = self.head
-        for _ in range(len(self) - 1):
+        for _ in range(self.size - 1):
             ll = ll.next
         ll.next = Node(val)
+        self._inc_size()
 
     def addAtIndex(self, index: int, val: int) -> None:
-        if not 0 <= index <= len(self):
+        if not 0 <= index <= self.size:
             return
         if index == 0:
             return self.addAtHead(val)
@@ -48,17 +54,23 @@ class MyLinkedList:
         for _ in range(index - 1):
             prev = prev.next
         prev.next = Node(val, prev.next)
+        self._inc_size()
 
     def deleteAtIndex(self, index: int) -> None:
-        if not 0 <= index < len(self):
+        if not 0 <= index < self.size:
             return
         if index == 0:
             self.head = self.head.next
+            self._dec_size()
             return
         prev = self.head
         for _ in range(index - 1):
             prev = prev.next
         prev.next = prev.next.next
+        self._dec_size()
+
+    def _dec_size(self):
+        self.size -= 1
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()

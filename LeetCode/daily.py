b9ea6474c523fdba5b876fc9b9483b2d35b0ee1f
kareemadesola <kareemadesola1999@gmail.com>
2022-06-16 08:45:03 +0100

    longest str chain using recursion

--- LeetCode/daily.py
+++ LeetCode/daily.py
@@ -358,6 +358,25 @@ def min_distance(word1: str, word2: str) -> int:
     return dp[-1]
 
 
+def longest_str_chain(words: List[str]) -> int:
+    s = set(words)
+    memo = {}
+
+    def rec(word):
+        if word not in s: return 0
+        if word in memo: return memo[word]
+        N = len(word)
+        mx = 0
+        for i in range(N):
+            mx = max(mx, rec(word[:i] + word[i + 1:]) + 1)
+        memo[word] = mx
+        return mx
+
+    for w in words:
+        rec(w)
+    return max(memo.values())
+
+
 class Test(unittest.TestCase):
     def test_minimum_total(self):
         self.assertEqual(minimum_total([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]), 11)

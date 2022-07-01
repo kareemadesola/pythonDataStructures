83a68fe24fa11fe64064ed41873c61b445c065ff
kareemadesola <kareemadesola1999@gmail.com>
2022-07-01 06:46:25 +0100

    refactor trie using helper method

--- LeetCode/random/june_22.py
+++ LeetCode/random/june_22.py
@@ -13,19 +13,20 @@ class Trie:
     def insert(self, word: str) -> None:
         curr = self.root
         for i in word:
-            if i not in curr.children:
-                curr.children[i] = TrieNode()
-            curr = curr.children[i]
+            curr = curr.children.setdefault(i, TrieNode())
+            # if i not in curr.children:
+            #     curr.children[i] = TrieNode()
+            # curr = curr.children[i]
         curr.end_of_word = True
 
     def search(self, word: str) -> bool:
-        node = self.search_helper(word)
+        node = self._search_helper(word)
         return node and node.end_of_word
 
     def starts_with(self, prefix: str) -> bool:
-        return bool(self.search_helper(prefix))
+        return bool(self._search_helper(prefix))
 
-    def search_helper(self, word: str):
+    def _search_helper(self, word: str):
         curr = self.root
         for i in word:
             if i not in curr.children:

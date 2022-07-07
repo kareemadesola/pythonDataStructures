184bb32e1116bafe7fbd47ecb80a3790ff9dd1ea
kareemadesola <kareemadesola1999@gmail.com>
2022-07-07 14:58:30 +0100

    97. Interleaving String
    using dp
    - Initialize dp array to False
    - Iterate from the ends
    - Check the following:
         - lengths are not exceeded
         - characters are equal
         - next value of index of array is True
    - return dp[0][0]
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/july_22.py
+++ LeetCode/daily/july_22.py
@@ -155,3 +155,22 @@ def is_interleave(s1: str, s2: str, s3: str) -> bool:
         return False
 
     return dfs(0, 0)
+
+
+# 2022-07-7, Thu, 14:08:24
+# time O(s1*s2)
+# space O(s1*s2)
+def is_interleave_dp(s1: str, s2: str, s3: str) -> bool:
+    if len(s1) + len(s2) != len(s3):
+        return False
+
+    dp = [[False] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+    dp[len(s1)][len(s2)] = True
+
+    for i in range(len(s1), -1, -1):
+        for j in range(len(s2), -1, -1):
+            if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                dp[i][j] = True
+            if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                dp[i][j] = True
+    return dp[0][0]

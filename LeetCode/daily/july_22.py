4d9436f1ad0b8980ad9a84750c84f43927e7aa40
kareemadesola <kareemadesola1999@gmail.com>
2022-07-07 13:53:15 +0100

    97. Interleaving String
    using memoization
    - return False if len s1 and s2!=s3
    - Initialize a set to store `False` values
    - Implement a dfs function check recursively
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/july_22.py
+++ LeetCode/daily/july_22.py
@@ -132,3 +132,26 @@ def fib_iter(n: int) -> int:
     for i in range(n):
         a, b = a + b, a
     return b
+
+
+# 2022-07-7, Thu, 08:40:54
+# time O(s1*s2)
+# space O(s1*s2)
+def is_interleave(s1: str, s2: str, s3: str) -> bool:
+    if len(s1) + len(s2) != len(s3):
+        return False
+    dp = set()
+
+    def dfs(i, j):
+        if i == len(s1) and j == len(s2):
+            return True
+        if (i, j) in dp:
+            return False
+        if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+            return True
+        if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+            return True
+        dp.add((i, j))
+        return False
+
+    return dfs(0, 0)

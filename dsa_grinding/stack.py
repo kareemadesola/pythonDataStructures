249e0ce25edecec1174a271754c0ef08872ba02e
kareemadesola <kareemadesola1999@gmail.com>
2022-08-30 12:00:06 +0100

    1598. Crawler Log Folder
    Without using stacks
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- dsa_grinding/stack.py
+++ dsa_grinding/stack.py
@@ -4,10 +4,8 @@ from typing import List
 def min_operations(logs: List[str]) -> int:
     count = 0
     for log in logs:
-        if count and log == "../":
-            count -= 1
-        elif log == "./":
-            continue
-        else:
+        if log != "../" and log != "./":
             count += 1
+        elif count and log == "../":
+            count -= 1
     return count

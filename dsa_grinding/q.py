2816e08d0577fbe2201994c643fd44732ab6c70f
kareemadesola <kareemadesola1999@gmail.com>
2022-09-02 21:07:55 +0100

    463. Island Perimeter
    DFS solution
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- dsa_grinding/q.py
+++ dsa_grinding/q.py
@@ -17,3 +17,21 @@ def island_perimeter(grid: List[List[int]]) -> int:
 
 def test_island_perimeter():
     assert island_perimeter([[0, 1, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0]]) == 16
+
+
+def island_perimeter_dfs(grid: List[List[int]]) -> int:
+    len_r, len_c = len(grid), len(grid[0])
+    visited = set()
+
+    def dfs(x: int, y: int) -> int:
+        if (x, y) in visited:
+            return 0
+        if not 0 <= x < len_r or not 0 <= y < len_c or grid[x][y] == 0:
+            return 1
+        visited.add((x, y))
+        return dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1)
+
+    for i in range(len_r):
+        for j in range(len_c):
+            if grid[i][j]:
+                return dfs(i, j)

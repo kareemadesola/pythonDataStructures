c812705e9cba82e56ae09628101db3470c5bc9e6
kareemadesola <kareemadesola1999@gmail.com>
2022-09-20 18:05:51 +0100

    718. Maximum Length of Repeated Subarray
    using dp
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/september_22.py
+++ LeetCode/daily/september_22.py
@@ -208,3 +208,14 @@ def find_duplicate(paths: List[str]) -> List[List[str]]:
             name, content = file.split('(')
             groups[content].append(f'{directory}/{name}')
         return [val for val in groups.values() if len(val) > 1]
+
+
+def find_length(nums1: List[int], nums2: List[int]) -> int:
+    m, n, res = len(nums1), len(nums2), 0
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if nums1[i - 1] == nums2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+                res = max(res, dp[i][j])
+    return res

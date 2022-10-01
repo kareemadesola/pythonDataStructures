757def1f147a206fe3f72847385298e58b83de0c
kareemadesola <kareemadesola1999@gmail.com>
2022-10-01 17:39:14 +0100

    string reversal
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

---
+++ recursion_simple_engineer/recursion.py
@@ -0,0 +1,14 @@
+"""
+What is the base case?
+When can I no longer continue (the laziest attempt)
+
+What is the smallest amount of work I can do in each iteration?
+Shrinks the problem space
+Smallest unit of work to contribute
+"""
+
+
+def reverse_string(val: str):
+    if val == '':
+        return ''
+    return reverse_string(val[1:]) + val[0]

3d8a82be931396e3e262fd1241227f38d9dc6a55
kareemadesola <kareemadesola1999@gmail.com>
2022-10-01 17:48:16 +0100

    can_sum problem naive recursion
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- alvin_dp_tutorial/can_sum.py
+++ alvin_dp_tutorial/can_sum.py
@@ -1,7 +1,7 @@
 import collections
 
 
-def can_sum(target_sum: int, numbers) -> bool:
+def can_sum_bfs(target_sum: int, numbers) -> bool:
     bfs = collections.deque([0])
     while bfs:
         curr = bfs.popleft()
@@ -15,5 +15,24 @@ def can_sum(target_sum: int, numbers) -> bool:
     return False
 
 
+# def test_can_sum_bfs():
+#     print(can_sum_bfs(22, [5, 7]))
+
+
+def can_sum(target_sum, numbers) -> bool:
+    # time O(numbers**(target_sum/min(numbers))
+    # space O(target_sum/min(numbers)
+    if target_sum == 0: return True
+    if target_sum < 0: return False
+
+    for number in numbers:
+        if can_sum(target_sum - number, numbers): return True
+    return False
+
+
 def test_can_sum():
-    print(can_sum(22, [5, 7]))
+    # assert can_sum(7, [2, 3])
+    assert can_sum(7, [5, 3, 4, 7])
+    # assert not can_sum(7, [2, 4, ])
+    # assert can_sum(8, [2, 3])
+    # assert not can_sum(300, [7, 14])

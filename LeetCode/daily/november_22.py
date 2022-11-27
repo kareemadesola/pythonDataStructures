9fd1932f140c457a283f562ab29ad6f4b33b9df8
Kareem Adesola <kareemadesola1999@gmail.com>
2022-11-27 12:56:22 +0100

    446. Arithmetic Slices II - Subsequence

--- LeetCode/daily/november_22.py
+++ LeetCode/daily/november_22.py
@@ -1,3 +1,4 @@
+import bisect
 import collections
 import random
 from typing import List, Optional
@@ -398,3 +399,13 @@ def exist(board: List[List[str]], word: str) -> bool:
             if dfs(i, j, len_):
                 return True
     return False
+
+
+def jobScheduling(startTime, endTime, profit):
+    jobs = sorted(zip(startTime, endTime, profit), key=lambda v: v[1])
+    dp = [[0, 0]]
+    for s, e, p in jobs:
+        i = bisect.bisect(dp, [s + 1]) - 1
+        if dp[i][1] + p > dp[-1][1]:
+            dp.append([e, dp[i][1] + p])
+    return dp[-1][1]

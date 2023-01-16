252761bef8901ca254e0f618fa19a40f09b9d81c
Kareem Adesola <kareemadesola1999@gmail.com>
2023-01-16 09:10:10 +0100

    746. Min Cost Climbing Stairs
    add dp bottom up and modify cost array

--- neetcode_dp.py
+++ neetcode_dp.py
@@ -1,5 +1,15 @@
+from typing import List
+
+
 def climb_stairs(n: int) -> int:
     tmp, res = 0, 1
     for _ in range(n):
         tmp, res = res, tmp + res
     return res
+
+
+def min_cost_climbing_stairs(cost: List[int]) -> int:
+    cost.append(0)
+    for i in range(len(cost) - 3, -1, -1):
+        cost[i] += min(cost[i + 1], cost[i + 2])
+    return min(cost[0], cost[1])

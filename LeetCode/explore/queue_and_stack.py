7c95d82af863cb275ade975875a6d3224b95e550
Kareem Adesola <kareemadesola1999@gmail.com>
2023-01-18 19:53:34 +0100

    200. Number of Islands
    use dfs

--- LeetCode/explore/queue_and_stack.py
+++ LeetCode/explore/queue_and_stack.py
@@ -244,3 +244,23 @@ def eval_rpn(tokens: List[str]) -> int:
 
 def test_eval_rpn():
     assert eval_rpn(["4", "13", "5", "/", "+"]) == 6
+
+
+def numIslands(grid):
+    def dfs(i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return 0
+
+        grid[i][j] = '#'
+        dfs(i + 1, j)
+        dfs(i - 1, j)
+        dfs(i, j + 1)
+        dfs(i, j - 1)
+        return 1
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '1':
+                count += dfs(i, j)
+    return count

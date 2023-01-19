17d36d7c49d2e627e378689b07ae3e06cf580ca0
Kareem Adesola <kareemadesola1999@gmail.com>
2023-01-19 16:05:53 +0100

    133. Clone Graph
    use dfs to deep copy

--- LeetCode/explore/queue_and_stack.py
+++ LeetCode/explore/queue_and_stack.py
@@ -264,3 +264,26 @@ def numIslands(grid):
             if grid[i][j] == '1':
                 count += dfs(i, j)
     return count
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+def clone_graph(node: Node) -> Node:
+    old_to_new = {}
+
+    def dfs(node: Node):
+        if node in old_to_new:
+            return old_to_new[node]
+
+        copy = Node(node.val)
+        old_to_new[node] = copy
+        for neighbour in node.neighbors:
+            copy.neighbors.append(dfs(neighbour))
+        return copy
+
+    return dfs(node) if node else None

cbfe59e9f4b4e9cf80cbb8c1d1f0a7e19b20b995
kareemadesola <kareemadesola1999@gmail.com>
2023-01-31 06:44:54 +0100

    278. First Bad Version
    use lower bound
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -61,3 +61,14 @@ def first_bad_version(n: int) -> int:
             r = mid - 1
         else:
             l = mid + 1
+
+
+def first_bad_version_l_bound(n: int) -> int:
+    l, r = 1, n
+    while l < r:
+        mid = l + (r - l) // 2
+        if is_bad_version(mid):
+            r = mid
+        else:
+            l = mid + 1
+    return l

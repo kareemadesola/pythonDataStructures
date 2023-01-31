70b66cc757315b99d1ac4cd4e54ac681f03b0a2e
kareemadesola <kareemadesola1999@gmail.com>
2023-01-31 05:40:14 +0100

    33. Search in Rotated Sorted Array
    add checks for boundaries
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -23,3 +23,24 @@ def my_sqrt(x: int) -> int:
         else:
             r = mid
     return l - 1 if x > 1 else x
+
+
+def search_rotated(nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return mid
+        # check right portion
+        if nums[mid] < nums[l]:
+            if nums[mid] > target or nums[r] < target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        # check left portion
+        else:
+            if nums[mid] < target or nums[l] > target:
+                l = mid + 1
+            else:
+                r = mid - 1
+    return -1

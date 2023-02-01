55db10cf72d7f16ebb927735b26ad76cbdb012ed
kareemadesola <kareemadesola1999@gmail.com>
2023-02-01 22:45:23 +0100

    162. Find Peak Element
    make r = len(nums) - 1
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -72,3 +72,14 @@ def first_bad_version_l_bound(n: int) -> int:
         else:
             l = mid + 1
     return l
+
+
+def find_peak_element(nums: List[int]) -> int:
+    l, r = 0, len(nums) - 1
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] > nums[mid + 1]:
+            r = mid
+        else:
+            l = mid + 1
+    return l

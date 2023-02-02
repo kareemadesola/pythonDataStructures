f2ec19d8291b1113129c22d84bf8d781ee8f75c4
kareemadesola <kareemadesola1999@gmail.com>
2023-02-02 07:26:38 +0100

    153. Find Minimum in Rotated Sorted Array
    get the no of rotations
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -83,3 +83,15 @@ def find_peak_element(nums: List[int]) -> int:
         else:
             l = mid + 1
     return l
+
+
+def findMin(nums: List[int]) -> int:
+    # get no of rotations
+    l, r = 0, len(nums) - 1
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] > nums[r]:
+            l = mid + 1
+        else:
+            r = mid
+    return nums[l]

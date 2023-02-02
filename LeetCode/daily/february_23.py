04956641c6966a3b29aac2ca5a7bab1d1f021ab0
kareemadesola <kareemadesola1999@gmail.com>
2023-02-02 13:51:54 +0100

    953. Verifying an Alien Dictionary
    add break condition to optimize
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/february_23.py
+++ LeetCode/daily/february_23.py
@@ -1,4 +1,5 @@
 import math
+from typing import List
 
 
 def gcdOfStrings(str1: str, str2: str) -> str:
@@ -6,3 +7,25 @@ def gcdOfStrings(str1: str, str2: str) -> str:
         return ''
     mx = math.gcd(len(str1), len(str2))
     return str1[:mx]
+
+
+def isAlienSorted(words: List[str], order: str) -> bool:
+    # adapted from https://leetcode.com/problems/verifying-an-alien-dictionary/description/
+
+    # time O(N) => N is the total length of characters in words
+    # space O(1) => O(26) == O(1)
+
+    # dict comprehension
+    order_to_idx = {char: idx for idx, char in enumerate(order)}
+
+    for i in range(len(words) - 1):
+        for j in range(len(words[i])):
+            # case example 'leet' 'leetcode'
+            if j >= len(words[i + 1]):
+                return False
+            # compare character of adjacent words
+            if words[i][j] != words[i + 1][j]:
+                if order_to_idx[words[i][j]] > order_to_idx[words[i + 1][j]]:
+                    return False
+                break
+    return True

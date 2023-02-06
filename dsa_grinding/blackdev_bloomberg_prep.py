7b204004ff69c644677287e19966382928b847e0
Kareem Adesola <kareemadesola1999@gmail.com>
2023-02-06 21:53:39 +0100

    2. Add Two Numbers
    using all at once

--- dsa_grinding/blackdev_bloomberg_prep.py
+++ dsa_grinding/blackdev_bloomberg_prep.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Optional
+
+from LeetCode.explore.linked_list import ListNode
 
 
 def twoSum(nums: List[int], target: int) -> List[int]:
@@ -10,3 +12,23 @@ def twoSum(nums: List[int], target: int) -> List[int]:
         if target - val in seen:
             return [idx, seen[target - val]]
         seen[val] = idx
+
+
+def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    # Mon, 06 Feb 2023  21:52:53
+    # time O(max(l1,l2))
+    # space O(max(l1,l2))
+    dummy = curr = ListNode()
+    carry = 0
+
+    while l1 or l2 or carry:
+        l1Val = l1.val if l1 else 0
+        l2Val = l2.val if l2 else 0
+        column_sum = l1Val + l2Val + carry
+        carry = column_sum // 10
+        curr.next = ListNode(column_sum % 10)
+        curr = curr.next
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+    return dummy.next

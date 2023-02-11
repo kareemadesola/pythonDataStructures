a69d413cf7afe7d4872419d1c50a2cc1f3e3c957
Kareem Adesola <kareemadesola1999@gmail.com>
2023-02-11 17:22:04 +0100

    7. Reverse Integer

--- dsa_grinding/blackdev_bloomberg_prep.py
+++ dsa_grinding/blackdev_bloomberg_prep.py
@@ -127,3 +127,9 @@ def reverse(x: int) -> int:
     if not res or not -2 ** 31 <= int(res) <= 2 ** 31 - 1:
         return 0
     return int(res) if x > 0 else -int(res)
+
+
+def reverse_better(x: int) -> int:
+    sign = 1 if x > 0 else -1
+    res = sign * int(str(abs(x))[::-1])
+    return res if -2 ** 31 <= res <= 2 ** 31 - 1 else 0

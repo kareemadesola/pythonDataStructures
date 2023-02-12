c9b396fbadca7a3375707d88ed7b19222970a5f3
Kareem Adesola <kareemadesola1999@gmail.com>
2023-02-12 20:56:33 +0100

    2477. Minimum Fuel Cost to Report to the Capital

--- LeetCode/daily/february_23.py
+++ LeetCode/daily/february_23.py
@@ -272,3 +272,24 @@ def shortestAlternatingPaths(n: int, redEdges: List[List[int]], blueEdges: List[
                     visited.add((nei, 'BLUE'))
                     q.append((nei, length + 1, 'BLUE'))
     return res
+
+
+def minimumFuelCost(roads: List[List[int]], seats: int) -> int:
+    adj = collections.defaultdict(list)
+    for src, dst in roads:
+        adj[src].append(dst)
+        adj[dst].append(src)
+
+    def dfs(node, parent):
+        nonlocal res
+        passengers = 0
+        for child in adj[node]:
+            if child != parent:
+                p = dfs(child, node)
+                passengers += p
+                res += math.ceil(p / seats)
+        return passengers + 1
+
+    res = 0
+    dfs(0, -1)
+    return res

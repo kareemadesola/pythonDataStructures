622527b47d1c05573209d075efb0fda86dc43aa2
Kareem Adesola <kareemadesola1999@gmail.com>
2023-02-15 00:09:15 +0100

    496. Next Greater Element I
    using monotonic decreasing stack

--- dsa_grinding/blackdev_bloomberg_prep.py
+++ dsa_grinding/blackdev_bloomberg_prep.py
@@ -161,3 +161,25 @@ def three_sum(nums: List[int]) -> List[List[int]]:
             res.append(list(i))
             seen.add(tuple(sorted(i)))
     return res
+
+
+def three_sum_better(nums: List[int]) -> List[List[int]]:
+    res = []
+    nums.sort()
+
+    for idx, val in enumerate(nums):
+        if idx > 0 and val == nums[idx - 1]:
+            continue
+        l, r = idx + 1, len(nums) - 1
+        while l < r:
+            tmp = val + nums[l] + nums[r]
+            if tmp > 0:
+                r -= 1
+            elif tmp < 0:
+                l += 1
+            else:
+                res.append([val, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+    return res

e7bb29740d382f1a2c468a6c458fb7494904197c
kareemadesola <kareemadesola1999@gmail.com>
2023-03-01 10:04:09 +0100

    912. Sort an Array
    using merge sort
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

---
+++ LeetCode/daily/march_23.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+def sortArray(nums: List[int]) -> List[int]:
+    def merge_sort(arr: List[int]):
+        if len(arr) <= 1:
+            return
+        mid = len(arr) // 2
+        l = arr[0: mid]
+        r = arr[mid:]
+        merge_sort(l)
+        merge_sort(r)
+        arr1_len, arr2_len = len(l), len(r)
+        i = j = 0
+        while i < arr1_len and j < arr2_len:
+            if l[i] < r[j]:
+                arr[i + j] = l[i]
+                i += 1
+            else:
+                arr[i + j] = r[j]
+                j += 1
+        while i < arr1_len:
+            arr[i + j] = l[i]
+            i += 1
+        while j < arr2_len:
+            arr[i + j] = r[j]
+            j += 1
+        return arr
+
+    return merge_sort(nums)

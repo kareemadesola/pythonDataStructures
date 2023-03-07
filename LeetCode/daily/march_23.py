e1424e64c9e295ce8f4c0efede8830b93af0c2d3
kareemadesola <kareemadesola1999@gmail.com>
2023-03-07 07:34:46 +0100

    1539. Kth Missing Positive Number
    binary search number - starting position -idx
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/march_23.py
+++ LeetCode/daily/march_23.py
@@ -60,3 +60,18 @@ def findKthPositive(arr: List[int], k: int) -> int:
         if res not in arr:
             k -= 1
     return res
+
+
+def findKthPositiveBetter(A, k):
+    l, r = 0, len(A)
+    while l < r:
+        m = (l + r) // 2
+        if A[m] - 1 - m < k:
+            l = m + 1
+        else:
+            r = m
+    return l + k
+
+
+if __name__ == '__main__':
+    findKthPositiveBetter([2, 3, 4, 7, 11], 5)

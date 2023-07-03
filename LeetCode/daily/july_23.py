be3fc32eadcb31177cde118195b024be5815076f
kareemadesola <kareemadesola1999@gmail.com>
2023-07-03 14:01:34 +0100

    2305. Fair Distribution of Cookies
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

---
+++ LeetCode/daily/july_23.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+def distributeCookies(cookies: List[int], k: int) -> int:
+    cur = [0] * k
+    n = len(cookies)
+
+    def dfs(i, zero_count):
+        if n - i < zero_count:
+            return float('inf')
+        if i == n:
+            return max(cur)
+
+        res = float('inf')
+        for j in range(k):
+            zero_count -= int(cur[j] == 0)
+            cur[j] += cookies[i]
+
+            res = min(res, dfs(i + 1, zero_count))
+
+            cur[j] -= cookies[i]
+            zero_count += int(cur[j] == 0)
+        return res
+
+    return dfs(0, k)

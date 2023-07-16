596b56c22f74e54fcf750a05c81268d53e2d2916
kareemadesola <kareemadesola1999@gmail.com>
2023-07-16 13:07:35 +0100

    1125. Smallest Sufficient Team
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/july_23.py
+++ LeetCode/daily/july_23.py
@@ -297,3 +297,35 @@ def maxValueBU(events: List[List[int]], k: int) -> int:
             nxt = bisect.bisect(starts, events[curr][1])
             dp[count][curr] = max(dp[count][curr + 1], dp[count - 1][nxt] + events[curr][2])
     return dp[k][0]
+
+
+def smallestSufficientTeam(req_skills: List[str], people: List[List[str]]) -> List[int]:
+    n = len(people)
+    m = len(req_skills)
+
+    skill_id = {}
+    for i, skill in enumerate(req_skills):
+        skill_id[skill] = i
+
+    skills_mask_of_person = [0] * n
+    for i in range(n):
+        for skill in people[i]:
+            skills_mask_of_person[i] |= 1 << skill_id[skill]
+
+    dp = [(1 << n) - 1] * (1 << m)
+    dp[0] = 0
+    for skills_mask in range(1, 1 << m):
+        for i in range(n):
+            smaller_skills_mask = skills_mask & ~skills_mask_of_person[i]
+            if smaller_skills_mask != skills_mask:
+                people_mask = dp[smaller_skills_mask] | (1 << i)
+                if people_mask.bit_count() < dp[skills_mask].bit_count():
+                    dp[skills_mask] = people_mask
+
+    answer_mask = dp[(1 << m) - 1]
+    res = []
+    for i in range(n):
+        if (answer_mask >> i) & 1:
+            res.append(i)
+
+    return res

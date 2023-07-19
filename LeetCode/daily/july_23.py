4caae25d8e613ad4166e46720c3a2afb8645b0e0
kareemadesola <kareemadesola1999@gmail.com>
2023-07-19 06:58:48 +0100

    435. Non-overlapping Intervals modified solution
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/july_23.py
+++ LeetCode/daily/july_23.py
@@ -395,12 +395,12 @@ class LRUCache:
 
 def eraseOverlapIntervals(intervals: List[List[int]]) -> int:
     intervals.sort()
-    start, end = intervals[0][0], intervals[0][1]
+    end = intervals[0][1]
     res = 0
     for i in range(1, len(intervals)):
         if intervals[i][0] < end:
-            start, end = min(start, intervals[i][0]), min(end, intervals[i][1])
+            end = min(end, intervals[i][1])
             res += 1
         else:
-            end = max(end, intervals[i][1])
+            end = intervals[i][1]
     return res

80050fb399fd122b1a1998087818fc3a6b32fdcc
kareemadesola <kareemadesola1999@gmail.com>
2023-08-01 03:44:45 +0100

    77. Combinations
    backtracking
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/september_23.py
+++ LeetCode/daily/september_23.py
@@ -7,3 +7,20 @@ def combine(n: int, k: int) -> List[List[int]]:
     # time O(r * nCr)
     # space O(r)
     return list(itertools.combinations(range(1, n + 1), k))
+
+
+def combineRC(n: int, k: int) -> List[List[int]]:
+    res = []
+    comb = []
+
+    def backtrack(start: int):
+        if len(comb) == k:
+            res.append(comb.copy())
+            return
+        for i in range(start, n + 1):
+            comb.append(i)
+            backtrack(i + 1)
+            comb.pop()
+
+    backtrack(1)
+    return res

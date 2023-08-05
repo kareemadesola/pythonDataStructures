e1c2e93275a50172d3388ed1f2f29b9f1073ebd1
kareemadesola <kareemadesola1999@gmail.com>
2023-08-05 08:01:42 +0100

    139. Word Break
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/august_23.py
+++ LeetCode/daily/august_23.py
@@ -74,3 +74,16 @@ def letterCombinations(digits: str) -> List[str]:
         backtrack(0)
     res = []
     return res
+
+
+def wordBreak(s: str, wordDict: List[str]) -> bool:
+    dp = [False] * (len(s) + 1)
+    dp[len(s)] = True
+
+    for i in range(len(s) - 1, -1, -1):
+        for w in wordDict:
+            if s[i : i + len(w)] == w:
+                dp[i] = dp[i + len(w)]
+            if dp[i]:
+                break
+    return dp[0]

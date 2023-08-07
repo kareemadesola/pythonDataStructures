ed6a0ca584bd756a6a9ce918a5f2a1e242d8bcba
kareemadesola <kareemadesola1999@gmail.com>
2023-08-07 08:56:56 +0100

    595. Big Countries
    replace or with |
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -14,5 +14,5 @@ World = pd.DataFrame(
 
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df = world[(world["area"] >= 3_000_000) or (world["population"] >= 25_000_000)]
+    df = world[(world["area"] >= 3_000_000) | (world["population"] >= 25_000_000)]
     return df[["name", "population", "area"]]

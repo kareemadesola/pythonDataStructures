ee187daa308fb2e1068d9569960f7ae09c7338f5
kareemadesola <kareemadesola1999@gmail.com>
2023-08-08 00:48:57 +0100

    1757. Recyclable and Low Fat Products
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -16,3 +16,13 @@ World = pd.DataFrame(
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
     df = world[(world["area"] >= 3_000_000) | (world["population"] >= 25_000_000)]
     return df[["name", "population", "area"]]
+
+
+products = pd.DataFrame([], columns=["product_id", "low_fats", "recyclable"]).astype(
+    {"product_id": "int64", "low_fats": "category", "recyclable": "category"}
+)
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    df = products[(products["low_fats"] == "Y") & (products["recyclable"] == "Y")]
+    return df[["product_id"]]

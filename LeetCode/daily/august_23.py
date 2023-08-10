0a80c553de8c0b33a169118a659542e0040cc524
kareemadesola <kareemadesola1999@gmail.com>
2023-08-10 22:11:19 +0100

    81. Search in Rotated Sorted Array II
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/august_23.py
+++ LeetCode/daily/august_23.py
@@ -201,3 +201,24 @@ def minimizeMax(nums: List[int], p: int) -> int:
         else:
             l = mid + 1
     return l
+
+
+def search(nums: List[int], target: int) -> bool:
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        mid = l + (r - l) // 2
+        if nums[mid] == target:
+            return True
+        if nums[mid] > nums[l]:  # left sorted
+            if nums[l] <= target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        elif nums[mid] < nums[l]:  # right sorted
+            if nums[mid] < target <= nums[r]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        else:
+            l += 1
+    return False

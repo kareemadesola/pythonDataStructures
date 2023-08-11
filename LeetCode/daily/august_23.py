6e503184150f3b9f3bd00f26dc0cb52e821b92bc
kareemadesola <kareemadesola1999@gmail.com>
2023-08-11 18:13:35 +0100

    322. Coin Change
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/august_23.py
+++ LeetCode/daily/august_23.py
@@ -222,3 +222,13 @@ def search(nums: List[int], target: int) -> bool:
         else:
             l += 1
     return False
+
+
+def coinChange(coins: List[int], amount: int) -> int:
+    dp = [amount + 1] * (amount + 1)
+    dp[0] = 0
+    for a in range(1, amount + 1):
+        for c in coins:
+            if a - c >= 0:
+                dp[a] = min(dp[a], 1 + dp[a - c])
+    return dp[amount] if dp[amount] != amount + 1 else -1

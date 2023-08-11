3a710de6058f28d3825cc29a330be8d0df20b39c
kareemadesola <kareemadesola1999@gmail.com>
2023-08-11 10:03:22 +0100

    1527. Patients With a Condition
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -132,3 +132,18 @@ def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
 
     regex = r"^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com$"
     return users[users["mail"].str.match(regex)]
+
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    data = [
+        [1, "Daniel", "YFEV COUGH"],
+        [2, "Alice", ""],
+        [3, "Bob", "DIAB100 MYOP"],
+        [4, "George", "ACNE DIAB100"],
+        [5, "Alain", "DIAB201"],
+    ]
+    Patients = pd.DataFrame(
+        data, columns=["patient_id", "patient_name", "conditions"]
+    ).astype({"patient_id": "int64", "patient_name": "object", "conditions": "object"})
+
+    return patients[patients["conditions"].str.contains(r"\bDIAB1")]

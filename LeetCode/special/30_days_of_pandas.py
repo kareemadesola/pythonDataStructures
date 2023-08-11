e7134c4ab410a0592b870e51dd4819591dd062df
kareemadesola <kareemadesola1999@gmail.com>
2023-08-11 08:56:09 +0100

    1517. Find Users With Valid E-Mails
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -119,3 +119,22 @@ Users = pd.DataFrame(data, columns=["user_id", "name"]).astype(
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
     users["name"] = users["name"].str.capitalize()
     return users.sort_values(by="user_id")
+
+
+data = [
+    [1, "Winston", "winston@leetcode.com"],
+    [2, "Jonathan", "jonathanisgreat"],
+    [3, "Annabelle", "bella-@leetcode.com"],
+    [4, "Sally", "sally.come@leetcode.com"],
+    [5, "Marwan", "quarz#2020@leetcode.com"],
+    [6, "David", "david69@gmail.com"],
+    [7, "Shapiro", ".shapo@leetcode.com"],
+]
+Users = pd.DataFrame(data, columns=["user_id", "name", "mail"]).astype(
+    {"user_id": "int64", "name": "object", "mail": "object"}
+)
+
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    regex = r"^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\.com$"
+    return users[users["mail"].str.match(regex)]

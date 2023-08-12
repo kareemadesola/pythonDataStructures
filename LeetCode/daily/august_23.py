28e12f7cb7ae746ff3148b81f0c69ef635a9f79e
kareemadesola <kareemadesola1999@gmail.com>
2023-08-12 11:59:14 +0100

    63. Unique Paths II
    top down
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/august_23.py
+++ LeetCode/daily/august_23.py
@@ -253,3 +253,18 @@ def uniquePaths1d(m: int, n: int) -> int:
         row = new_row
 
     return row[0]
+
+
+def uniquePathsWithObstacles(obstacleGrid: List[List[int]]) -> int:
+    m, n = len(obstacleGrid), len(obstacleGrid[0])
+    dp = {(m - 1, n - 1): 1}
+
+    def dfs(r: int, c: int) -> int:
+        if (r, c) in dp:
+            return dp[(r, c)]
+        if r == m or c == n or obstacleGrid[r][c]:
+            return 0
+        dp[(r, c)] = dfs(r - 1, c) + dfs(r, c - 1)
+        return dp[(r, c)]
+
+    return dfs(0,0)

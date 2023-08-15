d869f6d0d80ebfa9482987821f430f85827d1189
kareemadesola <kareemadesola1999@gmail.com>
2023-08-15 06:37:30 +0100

    1795. Rearrange Products Table
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -185,3 +185,13 @@ def delete_duplicate_emails(person: pd.DataFrame) -> None:
     )
     person.sort_values(by="id", inplace=True)
     person.drop_duplicates(subset="email", inplace=True)
+
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    data = [[0, 95, 100, 105], [1, 70, None, 80]]
+    Products = pd.DataFrame(
+        data, columns=["product_id", "store1", "store2", "store3"]
+    ).astype(
+        {"product_id": "int64", "store1": "int64", "store2": "int64", "store3": "int64"}
+    )
+    return products.melt("product_id", var_name="store", value_name="price").dropna()

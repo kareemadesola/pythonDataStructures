bf04366167f9171faae12906c5130c4ab2957b3b
kareemadesola <kareemadesola1999@gmail.com>
2023-08-17 17:18:10 +0100

    511. Game Play Analysis I
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -280,5 +280,31 @@ def total_time(employees: pd.DataFrame) -> pd.DataFrame:
     res_df: pd.DataFrame = (
         employees.groupby(["emp_id", "event_day"])["diff"].sum().reset_index()
     )
-    res_df.rename({"event_day": "day", "diff": "total_time"}, inplace=True)
+    res_df.rename(columns={"event_day": "day", "diff": "total_time"}, inplace=True)
     return res_df
+
+
+def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
+    data = [
+        [1, 2, "2016-03-01", 5],
+        [1, 2, "2016-05-02", 6],
+        [2, 3, "2017-06-25", 1],
+        [3, 1, "2016-03-02", 0],
+        [3, 4, "2018-07-03", 5],
+    ]
+    Activity = pd.DataFrame(
+        data, columns=["player_id", "device_id", "event_date", "games_played"]
+    ).astype(
+        {
+            "player_id": "Int64",
+            "device_id": "Int64",
+            "event_date": "datetime64[ns]",
+            "games_played": "Int64",
+        }
+    )
+
+    grouped_df: pd.DataFrame = (
+        activity.groupby("player_id")["event_date"].min().reset_index()
+    )
+    grouped_df.rename(columns={"event_date": "first_login"}, inplace=True)
+    return grouped_df

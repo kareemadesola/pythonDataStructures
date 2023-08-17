364e9d0f517479a13953cadc0928092db123f1ce
kareemadesola <kareemadesola1999@gmail.com>
2023-08-17 17:30:46 +0100

    2356. Number of Unique Subjects Taught by Each Teacher
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -308,3 +308,15 @@ def game_analysis(activity: pd.DataFrame) -> pd.DataFrame:
     )
     grouped_df.rename(columns={"event_date": "first_login"}, inplace=True)
     return grouped_df
+
+
+def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
+    data = [[1, 2, 3], [1, 2, 4], [1, 3, 3], [2, 1, 1], [2, 2, 1], [2, 3, 1], [2, 4, 1]]
+    Teacher = pd.DataFrame(
+        data, columns=["teacher_id", "subject_id", "dept_id"]
+    ).astype({"teacher_id": "Int64", "subject_id": "Int64", "dept_id": "Int64"})
+    grouped_df: pd.DataFrame = (
+        teacher.groupby("teacher_id")["subject_id"].nunique().reset_index()
+    )
+    grouped_df.rename(columns={"subject_id": "cnt"}, inplace=True)
+    return grouped_df

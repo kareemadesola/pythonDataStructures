6c5dbf694e1475cf1ce76ea3b7b3c70384b76d05
kareemadesola <kareemadesola1999@gmail.com>
2023-08-19 02:11:42 +0100

    1693. Daily Leads and Partners
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -375,3 +375,36 @@ def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
         )
         .reset_index()
     )
+
+
+def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
+    data = [
+        ["2020-12-8", "toyota", 0, 1],
+        ["2020-12-8", "toyota", 1, 0],
+        ["2020-12-8", "toyota", 1, 2],
+        ["2020-12-7", "toyota", 0, 2],
+        ["2020-12-7", "toyota", 0, 1],
+        ["2020-12-8", "honda", 1, 2],
+        ["2020-12-8", "honda", 2, 1],
+        ["2020-12-7", "honda", 0, 1],
+        ["2020-12-7", "honda", 1, 2],
+        ["2020-12-7", "honda", 2, 1],
+    ]
+    DailySales = pd.DataFrame(
+        data, columns=["date_id", "make_name", "lead_id", "partner_id"]
+    ).astype(
+        {
+            "date_id": "datetime64[ns]",
+            "make_name": "object",
+            "lead_id": "Int64",
+            "partner_id": "Int64",
+        }
+    )
+    return (
+        daily_sales.groupby(["date_id", "make_name"])
+        .agg(
+            unique_leads=("lead_id", "nunique"),
+            unique_partners=("partner_id", "nunique"),
+        )
+        .reset_index()
+    )

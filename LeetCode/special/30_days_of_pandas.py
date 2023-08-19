e8c218cc244a87e5c36199959668155fa42bd774
kareemadesola <kareemadesola1999@gmail.com>
2023-08-19 02:53:21 +0100

    1050. Actors and Directors Who Cooperated At Least Three Times
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -408,3 +408,16 @@ def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
         )
         .reset_index()
     )
+
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    data = [[1, 1, 0], [1, 1, 1], [1, 1, 2], [1, 2, 3], [1, 2, 4], [2, 1, 5], [2, 1, 6]]
+    ActorDirector = pd.DataFrame(
+        data, columns=["actor_id", "director_id", "timestamp"]
+    ).astype({"actor_id": "int64", "director_id": "int64", "timestamp": "int64"})
+    stats = (
+        actor_director.groupby(["actor_id", "director_id"])
+        .agg(count=("director_id", "count"))
+        .reset_index()
+    )
+    return stats[stats["count"] >= 3][["actor_id", "director_id"]]

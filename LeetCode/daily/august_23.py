a227815aeaee26931a7f80f538f4ce2fa03b3b3b
kareemadesola <kareemadesola1999@gmail.com>
2023-08-21 11:17:52 +0100

    459. Repeated Substring Pattern
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/august_23.py
+++ LeetCode/daily/august_23.py
@@ -437,3 +437,13 @@ def updateMatrix(mat: List[List[int]]) -> List[List[int]]:
                 mat[n_r][n_c] = mat[r][c] + 1
                 q.append((n_r, n_c))
     return mat
+
+
+def repeatedSubstringPattern(s: str) -> bool:
+    n = len(s)
+    for i in range(1, n // 2 + 1):
+        if n % i == 0:
+            pattern = s[:i] * (n // i)
+            if pattern == s:
+                return True
+    return False

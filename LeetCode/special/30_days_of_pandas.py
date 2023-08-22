b4c6fbba452560f709510d26c1dfdbdee3a00a9e
kareemadesola <kareemadesola1999@gmail.com>
2023-08-22 14:03:16 +0100

    1378. Replace Employee ID With The Unique Identifier
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -421,3 +421,18 @@ def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
         .reset_index()
     )
     return stats[stats["count"] >= 3][["actor_id", "director_id"]]
+
+
+def replace_employee_id(
+    employees: pd.DataFrame, employee_uni: pd.DataFrame
+) -> pd.DataFrame:
+    data = [[1, "Alice"], [7, "Bob"], [11, "Meir"], [90, "Winston"], [3, "Jonathan"]]
+    Employees = pd.DataFrame(data, columns=["id", "name"]).astype(
+        {"id": "int64", "name": "object"}
+    )
+    data = [[3, 1], [11, 2], [90, 3]]
+    EmployeeUNI = pd.DataFrame(data, columns=["id", "unique_id"]).astype(
+        {"id": "int64", "unique_id": "int64"}
+    )
+    merged_df = employees.merge(employee_uni, on="id", how="left")
+    return merged_df[["unique_id", "name"]]

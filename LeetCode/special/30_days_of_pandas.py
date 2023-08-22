1b9fbcadb53e9b90847f42eb42ad1ed1c207a17c
kareemadesola <kareemadesola1999@gmail.com>
2023-08-22 18:22:02 +0100

    1280. Students and Examinations
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/special/30_days_of_pandas.py
+++ LeetCode/special/30_days_of_pandas.py
@@ -436,3 +436,19 @@ def replace_employee_id(
     )
     merged_df = employees.merge(employee_uni, on="id", how="left")
     return merged_df[["unique_id", "name"]]
+
+
+def students_and_examinations(
+    students: pd.DataFrame, subjects: pd.DataFrame, examinations: pd.DataFrame
+) -> pd.DataFrame:
+    examinations = (
+        examinations.groupby(["student_id", "subject_name"])
+        .agg(attended_exams=("subject_name", "count"))
+        .reset_index()
+    )
+    students = students.merge(subjects, how="cross")
+    merged_df = students.merge(
+        examinations, on=["student_id", "subject_name"], how="left"
+    )
+    merged_df = merged_df.fillna(0).sort_values(["student_id", "subject_name"])
+    return merged_df[["student_id", "student_name", "subject_name", "attended_exams"]]

b9758cd1324b466146235d7482c4ba14b18b6205
kareemadesola <kareemadesola1999@gmail.com>
2023-09-04 10:07:17 +0100

    153. Find Minimum in Rotated Sorted Array
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -151,3 +151,14 @@ def nextGreatestLetter(letters: List[str], target: str) -> str:
         else:
             r = mid
     return letters[l % len(letters)]
+
+
+def findMin(nums: List[int]) -> int:
+    l, r = 0, len(nums) - 1
+    while l < r:
+        mid = l + (r - l) // 2
+        if nums[mid] > nums[r]:
+            l = mid + 1
+        else:
+            r = mid
+    return l

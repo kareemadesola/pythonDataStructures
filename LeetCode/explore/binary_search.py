fd6ac989d3c691f4109468f36f32ff249ed2261f
kareemadesola <kareemadesola1999@gmail.com>
2023-09-05 07:46:10 +0100

    167. Two Sum II - Input Array Is Sorted
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -192,3 +192,22 @@ def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
             res.append(num)
             num_to_count[num] -= 1
     return res
+
+
+def twoSum(numbers: List[int], target: int) -> List[int]:
+    def binary_search(left, to_find):
+        l, r = left, len(numbers) - 1
+        while l <= r:
+            mid = l + (r - l) // 2
+            if numbers[mid] == to_find:
+                return mid + 1
+            elif numbers[mid] < to_find:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+
+    for i in range(len(numbers)):
+        res = binary_search(i + 1, target - numbers[i])
+        if res != -1:
+            return [i + 1, res]

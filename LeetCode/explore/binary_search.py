12ac74fb115b7e440d4bbb4e3ced16f27415adcb
kareemadesola <kareemadesola1999@gmail.com>
2023-09-08 19:22:42 +0100

    4. Median of Two Sorted Arrays
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_search.py
+++ LeetCode/explore/binary_search.py
@@ -226,3 +226,30 @@ def findDuplicate(nums: List[int]) -> int:
         slow2 = nums[slow2]
         if slow == slow2:
             return slow
+
+
+def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+    total = len(nums1) + len(nums2)
+    half = total // 2
+
+    if len(nums2) < len(nums1):
+        nums1, nums2 = nums2, nums1
+
+    l, r = 0, len(nums1) - 1
+    while True:
+        mid1 = (l + r) // 2
+        mid2 = half - mid1 - 2
+
+        nums1_left = nums1[mid1] if mid1 >= 0 else float("-inf")
+        nums1_right = nums1[mid1 + 1] if mid1 + 1 < len(nums1) else float("inf")
+        nums2_left = nums2[mid2] if mid2 >= 0 else float("-inf")
+        nums2_right = nums2[mid2 + 1] if mid2 + 1 < len(nums2) else float("inf")
+
+        if nums1_left <= nums2_right and nums2_left <= nums1_right:
+            if total % 2 == 0:
+                return (max(nums1_left, nums2_left) + min(nums1_right, nums2_right)) / 2
+            return min(nums1_right, nums2_right)
+        elif nums1_left > nums2_right:
+            r = mid1 - 1
+        else:
+            l = mid1 + 1

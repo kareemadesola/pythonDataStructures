52ad864bf733bf736a7a2465200235a24083143f
kareemadesola <kareemadesola1999@gmail.com>
2023-09-11 05:58:41 +0100

    94. Binary Tree Inorder Traversal Iterative
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/binary_tree.py
+++ LeetCode/explore/binary_tree.py
@@ -47,3 +47,17 @@ def inorderTraversal(root: Optional[TreeNode]) -> List[int]:
 
     dfs(root)
     return res
+
+
+def inorderTraversalIter(root: Optional[TreeNode]) -> List[int]:
+    stack = []
+    res = []
+    curr = root
+    while stack or curr:
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        curr = stack.pop()
+        res.append(curr.val)
+        curr = curr.right
+    return res

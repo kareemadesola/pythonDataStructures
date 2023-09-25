4cbcfdb1ee644884dd241059992b7220c434a7a8
kareemadesola <kareemadesola1999@gmail.com>
2023-09-25 18:48:53 +0100

    24. Swap Nodes in Pairs
    Iterative solution
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/recursion.py
+++ LeetCode/explore/recursion.py
@@ -1,4 +1,6 @@
-from typing import List
+from typing import List, Optional
+
+from LeetCode.explore.linked_list import ListNode
 
 
 def reverseString(s: List[str]) -> None:
@@ -13,3 +15,19 @@ def reverseString(s: List[str]) -> None:
         dfs(l + 1, r - 1)
 
     dfs(0, len(s) - 1)
+
+
+def swapPairsIter(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode(0, head)
+    prev, curr = dummy, head
+    while curr and curr.next:
+        next_pair = curr.next.next
+        second = curr.next
+
+        second.next = curr
+        curr.next = next_pair
+        prev.next = second
+
+        prev = curr
+        curr = next_pair
+    return dummy.next

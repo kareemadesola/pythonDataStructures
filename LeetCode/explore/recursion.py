dcbc26adc49394133337e7962fa3f1dcfaeb4c49
kareemadesola <kareemadesola1999@gmail.com>
2023-10-01 12:25:58 +0100

    21. Merge Two Sorted Lists
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/explore/recursion.py
+++ LeetCode/explore/recursion.py
@@ -153,3 +153,26 @@ def myPow(x: float, n: int) -> float:
         return val * dfs(val, pw - 1)
 
     return dfs(x, n) if n > 0 else 1 / dfs(x, -n)
+
+
+def mergeTwoLists(
+    list1: Optional[ListNode], list2: Optional[ListNode]
+) -> Optional[ListNode]:
+    dummy = curr = ListNode()
+    while list1 and list2:
+        if list1.val <= list2.val:
+            curr.next = list1
+            list1 = list1.next
+        else:
+            curr.next = list2
+            list2 = list2.next
+        curr = curr.next
+    while list1:
+        curr.next = list1
+        list1 = list1.next
+        curr = curr.next
+    while list2:
+        curr.next = list2
+        list2 = list2.next
+        curr = curr.next
+    return dummy.next

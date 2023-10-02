71c2e0b29b8c9dab5e80e1bc14d563da8de58d7b
kareemadesola <kareemadesola1999@gmail.com>
2023-10-02 10:44:45 +0100

    2038. Remove Colored Pieces if Both Neighbors are the Same Color
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/october_23.py
+++ LeetCode/daily/october_23.py
@@ -1,2 +1,24 @@
 def reverseWords(s: str) -> str:
     return " ".join(word[::-1] for word in s.split(" "))
+
+
+def winnerOfGame(colors: str) -> bool:
+    global_cnt_a = global_cnt_b = 0
+    local_cnt_a = local_cnt_b = 0
+
+    for char in colors:
+        if char == "A":
+            local_cnt_a += 1
+        else:
+            global_cnt_a += max(local_cnt_a - 2, 0)
+            local_cnt_a = 0
+    global_cnt_a += max(local_cnt_a - 2, 0)
+
+    for char in colors:
+        if char == "B":
+            local_cnt_b += 1
+        else:
+            global_cnt_b += max(local_cnt_b - 2, 0)
+            local_cnt_b = 0
+    global_cnt_b += max(local_cnt_b - 2, 0)
+    return global_cnt_a > global_cnt_b

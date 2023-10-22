c2226277f95a7d42661a4ceb0f4ec9cbbdf01b31
kareemadesola <kareemadesola1999@gmail.com>
2023-10-22 18:09:13 +0100

    contest 368
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

---
+++ LeetCode/Weekly/contest_368.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def minimumSum(nums: List[int]) -> int:
+    nums_len = len(nums)
+    global_min = float("inf")
+    for j in range(1, nums_len - 1):
+        pass
+
+
+if __name__ == "__main__":
+    test = minimumSum([5, 4, 8, 7, 10, 2])
+    print(test)

a34a6bfb326088044b572a7a0760d9d70ecf770a
kareemadesola <kareemadesola1999@gmail.com>
2023-10-22 18:08:41 +0100

    1793. Maximum Score of a Good Subarray
    using greedy approach
    
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/october_23.py
+++ LeetCode/daily/october_23.py
@@ -496,3 +496,22 @@ def backspaceCompareO1_space(s: str, t: str) -> bool:
         s_idx -= 1
         t_idx -= 1
     return True
+
+
+def constrainedSubsetSum(nums: List[int], k: int) -> int:
+    nums_len = len(nums)
+    l = r = k
+    res = nums[k]
+    curr_min = nums[k]
+
+    while l > 0 or r < nums_len - 1:
+        left = nums[l - 1] if l > 0 else 0
+        right = nums[r + 1] if r < nums_len - 1 else 0
+        if left > right:
+            l -= 1
+            curr_min = min(curr_min, left)
+        else:
+            r += 1
+            curr_min = min(curr_min, right)
+        res = max(res, curr_min * (r - l + 1))
+    return res

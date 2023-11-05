038e5583224ea71b61723bbe623048c4e3fa7413
kareemadesola <kareemadesola1999@gmail.com>
2023-11-05 21:09:33 +0100

    1535. Find the Winner of an Array Game
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/october_23.py
+++ LeetCode/daily/october_23.py
@@ -725,3 +725,38 @@ def findMode(root: Optional[TreeNode]) -> List[int]:
         if val == mode:
             res.append(key)
     return res
+
+
+def getWinner(arr: List[int], k: int) -> int:
+    max_element = max(arr)
+    curr = arr[0]
+    win_streak = 0
+    q = collections.deque(arr[1:])
+    while q:
+        opponent = q.popleft()
+        if curr > opponent:
+            win_streak += 1
+            q.append(opponent)
+        else:
+            q.append(curr)
+            curr = opponent
+            win_streak = 1
+        if win_streak == k or curr == max_element:
+            return curr
+
+
+def getWinnerAlt(arr: List[int], k: int) -> int:
+    max_element = max(arr)
+    curr = arr[0]
+    win_streak = 0
+
+    for i in range(1, len(arr)):
+        opponent = arr[i]
+        if curr > opponent:
+            win_streak += 1
+        else:
+            curr = opponent
+            win_streak = 1
+
+        if win_streak == k or curr == max_element:
+            return curr

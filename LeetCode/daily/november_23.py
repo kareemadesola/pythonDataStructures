8f76711afaae1d49ddfc3141db237b6532279107
kareemadesola <kareemadesola1999@gmail.com>
2023-11-16 13:53:46 +0100

    1846. Maximum Element After Decreasing and Rearranging

--- LeetCode/daily/november_23.py
+++ LeetCode/daily/november_23.py
@@ -189,3 +189,24 @@ def sortVowelsCountingSort(s: str) -> str:
             j += 1
 
     return ''.join(res)
+
+
+def maximumElementAfterDecrementingAndRearranging(arr: List[int]) -> int:
+    arr.sort()
+    n = len(arr)
+    arr[0] = 1
+    res = 1
+    for i in range(1, n):
+        res = min(res + 1, arr[i])
+    return res
+
+
+def maximumElementAfterDecrementingAndRearrangingAlt(arr: List[int]) -> int:
+    n = len(arr)
+    count = [0] * (n + 1)
+    for num in arr:
+        count[min(num, n)] += 1
+    res = 1
+    for i in range(2, n + 1):
+        res = min(res + count[i], i)
+    return res

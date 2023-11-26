defc38f1012f2f0ff12d964d83782249101b8926
kareemadesola <kareemadesola1999@gmail.com>
2023-11-26 17:00:42 +0100

    1274 - Number of Ships in a Rectangle

--- LeetCode/special/bloomberg_revison_23.py
+++ LeetCode/special/bloomberg_revison_23.py
@@ -1,3 +1,6 @@
+import random
+
+
 class UndergroundSystem:
 
     def __init__(self):
@@ -17,3 +20,44 @@ class UndergroundSystem:
     def getAverageTime(self, startStation: str, endStation: str) -> float:
         time, freq = self.average_time[(startStation, endStation)]
         return time / freq
+
+
+# """
+# This is Sea's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+class Sea:
+    def hasShips(self, topRight: 'Point', bottomLeft: 'Point') -> bool:
+        return random.choice([True, False])
+
+
+class Point:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Solution:
+    def countShips(self, sea: 'Sea', topRight: 'Point', bottomLeft: 'Point') -> int:
+        def dfs(t_r: Point, b_l: Point) -> int:
+            if t_r.x > b_l.x or t_r.y > b_l.y:
+                return 0
+
+            if not Sea.hasShips(t_r, b_l):
+                return 0
+
+            if t_r.x == t_r.y == b_l.x == b_l.y:
+                return 1
+            res = 0
+            mid_x, mid_y = (t_r.x - b_l.x) // 2, (t_r.y - b_l.y) // 2
+            # bottom left
+            res += dfs(Point(mid_x, mid_y), b_l)
+            # bottom_right
+            res += dfs(Point(t_r.x, mid_y), Point(mid_x + 1, b_l.y))
+            # top_left
+            res += dfs(Point(mid_x, t_r.y), Point(b_l.x, mid_y + 1))
+            # top_right
+            res += dfs(t_r, Point(mid_x + 1, mid_y + 1))
+            return res
+
+        return dfs(topRight, bottomLeft)

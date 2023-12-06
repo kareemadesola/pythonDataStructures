df9d4eeec8775718b09a1d6c56eb8efd8aed3d40
kareemadesola <kareemadesola1999@gmail.com>
2023-12-07 00:42:18 +0100

    1716. Calculate Money in Leetcode Bank

--- LeetCode/daily/december_23.py
+++ LeetCode/daily/december_23.py
@@ -19,3 +19,43 @@ def numberOfMatchesAlt(n: int) -> int:
             res += n // 2
             n = n // 2
     return res
+
+
+def totalMoney(n: int) -> int:
+    res = 0
+    div, mod = divmod(n, 7)
+
+    tmp = 0
+    for _ in range(div):
+        res += 28 + tmp
+        tmp += 7
+
+    tmp = div + 1
+    for _ in range(mod):
+        res += tmp
+        tmp += 1
+    return res
+
+
+def totalMoneyAlt(n: int) -> int:
+    res = 0
+    monday = 1
+    while n > 0:
+        for day in range(min(n, 7)):
+            res += day + monday
+        monday += 1
+        n -= 7
+    return res
+
+
+def totalMoneyOpt(n: int) -> int:
+    k = n // 7
+    f = 28
+    l = f + (k - 1) * 7
+    arith_sum = k * (f + l) // 2
+
+    rem = n % 7
+    monday = k + 1
+    for day in range(rem):
+        arith_sum += monday + day
+    return arith_sum

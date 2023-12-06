96b389448fba3c55d9036e6064488f4eb1a3f899
kareemadesola <kareemadesola1999@gmail.com>
2023-12-06 05:10:25 +0100

    1688. Count of Matches in Tournament
    simulation

--- LeetCode/daily/december_23.py
+++ LeetCode/daily/december_23.py
@@ -7,3 +7,15 @@ def arrayStringsAreEqual(word1: List[str], word2: List[str]) -> bool:
 
 def numberOfMatches(n: int) -> int:
     return n - 1
+
+
+def numberOfMatchesAlt(n: int) -> int:
+    res = 0
+    while n > 1:
+        if n % 2:
+            n = (n - 1) // 2 + 1
+            res += (n - 1) // 2
+        else:
+            res += n // 2
+            n = n // 2
+    return res

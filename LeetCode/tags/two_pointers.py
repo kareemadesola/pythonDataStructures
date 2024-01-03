e89c4c0b497eea3f9a94f4b5957269cbc51119d8
kareemadesola <kareemadesola1999@gmail.com>
2024-01-03 01:17:01 +0100

    27. Remove Element

--- LeetCode/tags/two_pointers.py
+++ LeetCode/tags/two_pointers.py
@@ -8,3 +8,12 @@ def removeDuplicates(nums: List[int]) -> int:
             l += 1
             nums[l] = nums[r]
     return l + 1
+
+
+def removeElement(self, nums: List[int], val: int) -> int:
+    l = 0
+    for r_val in nums:
+        if r_val != val:
+            nums[l] = r_val
+            l += 1
+    return l

ded7f3b9e07c820e17eb40224c2a309bfd957e2d
kareemadesola <kareemadesola1999@gmail.com>
2024-01-03 02:53:02 +0100

    88. Merge Sorted Array
    offset for indices

--- LeetCode/tags/two_pointers.py
+++ LeetCode/tags/two_pointers.py
@@ -28,13 +28,15 @@ def strStr(haystack: str, needle: str) -> int:
 
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-    while m > 0 and n > 0:
-        if nums1[m - 1] > nums2[n - 1]:
-            nums1[m + n - 1] = nums1[m - 1]
+    m -= 1
+    n -= 1
+    while m >= 0 and n >= 0:
+        if nums1[m] > nums2[n]:
+            nums1[m + n + 1] = nums1[m]
             m -= 1
         else:
-            nums1[m + n - 1] = nums2[n - 1]
+            nums1[m + n + 1] = nums2[n]
             n -= 1
-    while n > 0:
-        nums1[n - 1] = nums2[n - 1]
+    while n >= 0:
+        nums1[n] = nums2[n]
         n -= 1

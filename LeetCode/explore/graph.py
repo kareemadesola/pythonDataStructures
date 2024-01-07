ca460f16ef6aade81c75818d0f5b7e4dd42cf823
kareemadesola <kareemadesola1999@gmail.com>
2024-01-07 09:02:17 +0100

    implement UnionFind quick union

--- LeetCode/explore/graph.py
+++ LeetCode/explore/graph.py
@@ -17,8 +17,30 @@ class UnionFindQuickFind:
         return self.find(x) == self.find(y)
 
 
+class UnionFindQuickUnion:
+    def __init__(self, size: int):
+        self.root = [i for i in range(size)]
+        self.size = size
+
+    def find(self, x: int) -> int:
+        while x != self.root[x]:
+            x = self.root[x]
+        return x
+
+    def union(self, x: int, y: int):
+        x_root = self.find(x)
+        y_root = self.find(y)
+
+        if x_root != y_root:
+            self.root[y_root] = x_root
+
+    def is_connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+
 if __name__ == '__main__':
-    uf = UnionFindQuickFind(10)
+    # uf = UnionFindQuickFind(10)
+    uf = UnionFindQuickUnion(10)
     # 1-2-5-6-7 3-8-9 4
     uf.union(1, 2)
     uf.union(2, 5)

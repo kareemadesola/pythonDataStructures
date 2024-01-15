f07b1a3def09a5c56379fe49e1c9e142722f2a8c
kareemadesola <kareemadesola1999@gmail.com>
2024-01-15 21:24:10 +0100

    399. Evaluate Division

--- LeetCode/explore/graph/practice.py
+++ LeetCode/explore/graph/practice.py
@@ -86,3 +86,42 @@ class UnionFind:
             self.root[root_y] = root_x
             self.rank[root_x] += 1
         self.count -= 1
+
+
+def calcEquation(equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+    gid_weight = {}
+
+    def find(node_id):
+        if node_id not in gid_weight:
+            gid_weight[node_id] = (node_id, 1)
+        group_id, node_weight = gid_weight[node_id]
+
+        if group_id == node_id:
+            return gid_weight[node_id]
+        new_group_id, group_weight = find(group_id)
+        gid_weight[node_id] = (new_group_id, node_weight * group_weight)
+        return gid_weight[node_id]
+
+    def union(dividend, divisor, value):
+        dividend_gid, dividend_weight = find(dividend)
+        divisor_gid, divisor_weight = find(divisor)
+
+        if dividend_gid != divisor_gid:
+            gid_weight[dividend_gid] = (divisor_gid, divisor_weight * value / dividend_weight)
+
+    for (dividend, divisor), value in zip(equations, values):
+        union(dividend, divisor, value)
+
+    res = []
+    for (dividend, divisor) in queries:
+        if dividend not in gid_weight or divisor not in gid_weight:
+            res.append(-1.0)
+        else:
+            dividend_gid, dividend_weight = find(dividend)
+            divisor_gid, divisor_weight = find(divisor)
+
+            if dividend_gid != divisor_gid:
+                res.append(-1.0)
+            else:
+                res.append(dividend_weight / divisor_weight)
+    return res

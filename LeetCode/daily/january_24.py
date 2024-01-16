7162af3904955cdf5f3651e97774f0734f0ce02d
kareemadesola <kareemadesola1999@gmail.com>
2024-01-16 05:29:51 +0100

    380. Insert Delete GetRandom O(1)

--- LeetCode/daily/january_24.py
+++ LeetCode/daily/january_24.py
@@ -1,6 +1,7 @@
 import bisect
 import collections
 import heapq
+import random
 from bisect import bisect_left
 from collections import defaultdict, Counter
 from math import ceil
@@ -425,3 +426,32 @@ def findWinnersAlt(matches: List[List[int]]) -> List[List[int]]:
         elif count == 1:
             one_loss.append(player)
     return [sorted(zero_losses), sorted(one_loss)]
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.array = []
+        self.hash_map = {}
+
+    def insert(self, val: int) -> bool:
+        if val in self.hash_map:
+            return False
+        self.hash_map[val] = len(self.array)
+        self.array.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.hash_map:
+            return False
+        index = self.hash_map[val]
+        last_element = self.array[-1]
+        self.array[index] = last_element
+        self.hash_map[last_element] = index
+
+        self.array.pop()
+        del self.hash_map[val]
+        return True
+
+    def getRandom(self) -> int:
+        return random.choice(self.array)

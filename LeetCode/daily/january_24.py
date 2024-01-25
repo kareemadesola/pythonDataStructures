25d2df8465cca6a62616f06d03facd794e51f873
kareemadesola <kareemadesola1999@gmail.com>
2024-01-25 22:22:21 +0100

    1143. Longest Common Subsequence

--- LeetCode/daily/january_24.py
+++ LeetCode/daily/january_24.py
@@ -623,3 +623,33 @@ def pseudoPalindromicPaths(root: Optional[TreeNode]) -> int:
     res = 0
     dfs(root)
     return res
+
+
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    m, n = len(text1), len(text2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+    return dp[0][0]
+
+
+def longestCommonSubsequenceOpt(text1: str, text2: str) -> int:
+    if len(text2) > len(text1):
+        text1, text2 = text2, text1
+    m, n = len(text1), len(text2)
+    prev_dp = [0] * (n + 1)
+
+    for i in range(m - 1, -1, -1):
+        dp = [0] * (m + 1)
+        for j in range(n - 1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[j] = 1 + prev_dp[j + 1]
+            else:
+                dp[j] = max(dp[j + 1], prev_dp[j])
+        prev_dp = dp
+    return prev_dp[0]

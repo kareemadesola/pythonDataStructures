1e21210cef8233ef4fd521e5093f2c91c9544307
kareemadesola <kareemadesola1999@gmail.com>
2024-01-28 11:44:17 +0100

    629. K Inverse Pairs Array

--- LeetCode/daily/january_24.py
+++ LeetCode/daily/january_24.py
@@ -727,3 +727,20 @@ def findPathsNC(m: int, n: int, maxMove: int, startRow: int, startColumn: int) -
                     dp[r][c] = (dp[r][c] + prev_dp[r][c + 1]) % MOD
         prev_dp = dp
     return prev_dp[startRow][startColumn]
+
+
+def kInversePairs(n: int, k: int) -> int:
+    MOD = 10 ** 9 + 7
+    prev = [0] * (k + 1)
+    prev[0] = 1
+
+    for N in range(1, n + 1):
+        curr = [0] * (k + 1)
+        total = 0
+        for K in range(0, k + 1):
+            if K >= N:
+                total -= prev[K - N]
+            total = (total + prev[K]) % MOD
+            curr[K] = total
+        prev = curr
+    return prev[k]

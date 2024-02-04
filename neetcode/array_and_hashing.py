bdae6b6a227fa66fa96b20a6d7ad5328bdeffc8c
kareemadesola <kareemadesola1999@gmail.com>
2024-02-04 23:23:20 +0100

    238. Product of Array Except Self

--- neetcode/array_and_hashing.py
+++ neetcode/array_and_hashing.py
@@ -46,3 +46,19 @@ def topKFrequent(nums: List[int], k: int) -> List[int]:
     for _ in range(k):
         res.append(heapq.heappop(heap)[1])
     return res
+
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    n = len(nums)
+    res = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= suffix
+        suffix *= nums[i]
+    return res

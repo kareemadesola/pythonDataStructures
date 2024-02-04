f1698d341d50497d905e28e25dd419729d30889e
kareemadesola <kareemadesola1999@gmail.com>
2024-02-04 11:59:17 +0100

    1043. Partition Array for Maximum Sum

--- LeetCode/daily/february_24.py
+++ LeetCode/daily/february_24.py
@@ -10,3 +10,20 @@ def divideArray(nums: List[int], k: int) -> List[List[int]]:
             return []
         res.append(tmp)
     return res
+
+
+def maxSumAfterPartitioning(arr, k):
+    N = len(arr)
+    K = k + 1
+
+    dp = [0] * K
+
+    for start in range(N - 1, -1, -1):
+        currMax = 0
+        end = min(N, start + k)
+
+        for i in range(start, end):
+            currMax = max(currMax, arr[i])
+            dp[start % K] = max(dp[start % K], dp[(i + 1) % K] + currMax * (i - start + 1))
+
+    return dp[0]

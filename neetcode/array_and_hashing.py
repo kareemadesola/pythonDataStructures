b5cd6457f20ba782d88f323b12bcd7171b0fc5a2
kareemadesola <kareemadesola1999@gmail.com>
2024-02-04 20:53:01 +0100

    242. Valid Anagram

--- neetcode/array_and_hashing.py
+++ neetcode/array_and_hashing.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 
@@ -8,3 +9,13 @@ def containsDuplicate(nums: List[int]) -> bool:
             return True
         seen.add(val)
     return False
+
+
+def isAnagram(s: str, t: str) -> bool:
+    s_cnt = Counter(s)
+    for t_val in t:
+        s_cnt[t_val] -= 1
+
+    for val in s_cnt.values():
+        if val: return False
+    return True

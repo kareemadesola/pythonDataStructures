8ebacdd18cdfffed197acf1bb4ac157c09e41953
kareemadesola <kareemadesola1999@gmail.com>
2024-02-06 22:58:13 +0100

    125. Valid Palindrome

--- neetcode/array_and_hashing.py
+++ neetcode/array_and_hashing.py
@@ -106,3 +106,17 @@ def longestConsecutiveAlt(nums: List[int]) -> int:
             length += 1
         longest = max(longest, length)
     return longest
+
+
+def isPalindrome(s: str) -> bool:
+    res = []
+    for char in s:
+        if char.isalnum():
+            res.append(char.lower())
+    l, r = 0, len(res) - 1
+    while l < r:
+        if res[l] != res[r]:
+            return False
+        l += 1
+        r -= 1
+    return True

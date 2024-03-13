443c490ebcc589be48b3c85d8966aebf731b7c0d
kareemadesola <kareemadesola1999@gmail.com>
2024-03-13 21:49:57 +0100

    move to correct file
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/march_24.py
+++ LeetCode/daily/march_24.py
@@ -35,3 +35,54 @@ def getCommonAlt(nums1: List[int], nums2: List[int]) -> int:
 def customSortString(order: str, s: str) -> str:
     char_to_idx = {val: idx for idx, val in enumerate(order)}
     return "".join(sorted(s, key=lambda x: char_to_idx[x] if x in char_to_idx else 201))
+
+
+def removeZeroSumSublists(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = start = ListNode(next=head)
+    while start:
+        prefix_sum = 0
+        end = start.next
+        while end:
+            prefix_sum += end.val
+            if prefix_sum == 0:
+                start.next = end.next
+            end = end.next
+        start = start.next
+    return dummy.next
+
+
+def removeZeroSumSublistsAlt(head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = curr = ListNode(next=head)
+    prefix_sum = 0
+    prefix_sum_to_node = {}
+
+    while curr:
+        prefix_sum += curr.val
+        prefix_sum_to_node[prefix_sum] = curr
+        curr = curr.next
+
+    curr = dummy
+    prefix_sum = 0
+    while curr:
+        prefix_sum += curr.val
+        curr.next = prefix_sum_to_node[prefix_sum].next
+        curr = curr.next
+    return dummy.next
+
+
+def pivotInteger(n: int) -> int:
+    def sum_n(first: int, last: int) -> int:
+        n = last - first + 1
+        return (n * (first + last)) // 2
+
+    l, r = 1, n
+    while l < r:
+        mid = l + (r - l) // 2
+        l_sum = sum_n(1, mid)
+        r_sum = sum_n(mid, n)
+
+        if l_sum < r_sum:
+            l = mid + 1
+        else:
+            r = mid
+    return l if sum_n(1, l) == sum_n(l, n) else -1

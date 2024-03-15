679ed5953c3266abcedd9b73c8186a98522ad73e
kareemadesola <kareemadesola1999@gmail.com>
2024-03-15 20:01:43 +0100

    238. Product of Array Except Self
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/march_24.py
+++ LeetCode/daily/march_24.py
@@ -104,3 +104,20 @@ def numSubarraysWithSum(nums: List[int], goal: int) -> int:
         return res
 
     return num_subarray_less_equal(goal) - num_subarray_less_equal(goal - 1)
+
+
+def productExceptSelf(nums: List[int]) -> List[int]:
+    n = len(nums)
+    res = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= suffix
+        suffix *= nums[i]
+
+    return res

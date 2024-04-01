acb9421890d4de3bd3afe4f6aee7cf5b22f1bcd5
kareemadesola <kareemadesola1999@gmail.com>
2024-04-01 17:06:54 +0100

    3. Longest Substring Without Repeating Characters
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- neetcode/sliding_window.py
+++ neetcode/sliding_window.py
@@ -20,3 +20,27 @@ def maxProfitAlt(prices: List[int]) -> int:
             l = r
         res = max(res, prices[r] - prices[l])
     return res
+
+
+def lengthOfLongestSubstring(s: str) -> int:
+    seen = set()
+    l, n = 0, len(s)
+    res = 0
+    for r in range(n):
+        while s[r] in seen:
+            seen.remove(s[l])
+            l += 1
+        seen.add(s[r])
+        res = max(res, r - l + 1)
+    return res
+
+
+def lengthOfLongestSubstringAlt(s: str) -> int:
+    char_to_idx = {}
+    res = l = 0
+    for r, val in enumerate(s):
+        if val in char_to_idx and char_to_idx[val] >= l:
+            l = char_to_idx[val] + 1
+        char_to_idx[val] = r
+        res = max(res, r - l + 1)
+    return res

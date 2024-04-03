b5faea87e64d4191d00ef63d4a5714584d6bb445
kareemadesola <kareemadesola1999@gmail.com>
2024-04-03 14:50:45 +0100

    205. Isomorphic Strings
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/april_24.py
+++ LeetCode/daily/april_24.py
@@ -14,6 +14,13 @@ def lengthOfLastWordAlt(s: str) -> int:
     return end - start
 
 
-if __name__ == '__main__':
-    s = "   fly me   to   the moon  "
-    lengthOfLastWordAlt(s)
+def isIsomorphic(s: str, t: str) -> bool:
+    s_to_t, t_to_s = {}, {}
+    n = len(s)
+    for i in range(n):
+        if s[i] in s_to_t and s_to_t[s[i]] != t[i] \
+                or t[i] in t_to_s and t_to_s[t[i]] != s[i]:
+            return False
+        s_to_t[s[i]] = t[i]
+        t_to_s[t[i]] = s[i]
+    return True

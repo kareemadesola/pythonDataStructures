bed3d7aa28df9db9c9d2f0c0b63ce986f180ad5c
kareemadesola <kareemadesola1999@gmail.com>
2024-04-03 15:08:32 +0100

    79. Word Search
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/april_24.py
+++ LeetCode/daily/april_24.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def lengthOfLastWord(s: str) -> int:
     return len(s.strip().split(' ')[-1])
 
@@ -24,3 +27,27 @@ def isIsomorphic(s: str, t: str) -> bool:
         s_to_t[s[i]] = t[i]
         t_to_s[t[i]] = s[i]
     return True
+
+
+def exist(board: List[List[str]], word: str) -> bool:
+    m, n = len(board), len(board[0])
+    path = set()
+    def backtrack(r:int, c:int, curr:int)->bool:
+        if curr == len(word):
+            return True
+        if not 0 <= r < m or not 0 <= c < n \
+                or (r, c) in path or board[r][c] != word[curr]:
+            return False
+        path.add((r, c))
+        res = backtrack(r + 1, c, curr + 1) \
+              or backtrack(r - 1, c, curr + 1) \
+              or backtrack(r, c + 1, curr + 1) \
+              or backtrack(r, c - 1, curr + 1)
+        path.remove((r, c))
+        return res
+
+    for i in range(m):
+        for j in range(n):
+            if board[i][j] == word[0] and backtrack(i, j, 0):
+                return True
+    return False

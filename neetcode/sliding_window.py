8882da4d8f27d368784bc8fb3335e1fb7a5a53c8
kareemadesola <kareemadesola1999@gmail.com>
2024-04-05 11:42:12 +0100

    567. Permutation in String
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- neetcode/sliding_window.py
+++ neetcode/sliding_window.py
@@ -72,3 +72,36 @@ def characterReplacementAlt(s: str, k: int) -> int:
             l += 1
         res = max(res, r - l + 1)
     return res
+
+
+def checkInclusion(s1: str, s2: str) -> bool:
+    if len(s1) > len(s2):
+        return False
+    s1_cnt, s2_cnt = [0] * 26, [0] * 26
+    for i in range(len(s1)):
+        s1_cnt[ord(s1[i]) - ord('a')] += 1
+        s2_cnt[ord(s2[i]) - ord('a')] += 1
+
+    matches = 0
+    for i in range(26):
+        matches += (1 if s1_cnt[i] == s2_cnt[i] else 0)
+
+    l = 0
+    for r in range(len(s1), len(s2)):
+        if matches == 26:
+            return True
+        index = ord(s2[r]) - ord('a')
+        s2_cnt[index] += 1
+        if s1_cnt[index] == s2_cnt[index]:
+            matches += 1
+        elif s1_cnt[index] + 1 == s2_cnt[index]:
+            matches -= 1
+
+        index = ord(s2[l]) - ord('a')
+        s2_cnt[index] -= 1
+        if s1_cnt[index] == s2_cnt[index]:
+            matches += 1
+        elif s1_cnt[index] - 1 == s2_cnt[index]:
+            matches -= 1
+        l += 1
+    return matches == 26

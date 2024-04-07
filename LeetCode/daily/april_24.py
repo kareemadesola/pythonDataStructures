6eb3ad9179da898f44899a709ecc051a93e1307e
kareemadesola <kareemadesola1999@gmail.com>
2024-04-07 14:13:38 +0100

    678. Valid Parenthesis String
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/april_24.py
+++ LeetCode/daily/april_24.py
@@ -74,3 +74,22 @@ def makeGood(s: str) -> str:
         else:
             stack.append(s[i])
     return ''.join(stack)
+
+
+def checkValidString(s: str) -> bool:
+    left_min = left_max = 0
+    for char in s:
+        if char == '(':
+            left_min += 1
+            left_max += 1
+        elif char == ')':
+            left_min -= 1
+            left_max -= 1
+        else:
+            left_min -= 1
+            left_max += 1
+        if left_max < 0:
+            return False
+        if left_min < 0:  # (*)(
+            left_min = 0
+    return left_min == 0

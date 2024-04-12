13e18f049e8401649be7b99c2b83f200d4b504ca
kareemadesola <kareemadesola1999@gmail.com>
2024-04-12 07:17:37 +0100

    42. Trapping Rain Water
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- LeetCode/daily/april_24.py
+++ LeetCode/daily/april_24.py
@@ -147,3 +147,18 @@ def deckRevealedIncreasing(deck: List[int]) -> List[int]:
         if q:
             q.append(q.popleft())
     return res
+
+
+def trap(height: List[int]) -> int:
+    l, r = 0, len(height) - 1
+    res = l_max = r_max = 0
+    while l <= r:
+        if l_max < r_max:
+            l_max = max(l_max, height[l])
+            res += l_max - height[l]
+            l += 1
+        else:
+            r_max = max(r_max, height[r])
+            res += r_max - height[r]
+            r -= 1
+    return res

06188e17c5d35dce5664cf01ea6574bfed42bc95
kareemadesola <kareemadesola1999@gmail.com>
2024-04-15 11:56:47 +0100

    155. Min Stack
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- neetcode/stack.py
+++ neetcode/stack.py
@@ -1,3 +1,6 @@
+from typing import List, Tuple
+
+
 def isValid(s: str) -> bool:
     stack = []
     close_to_open = {')': '(', ']': '[', '}': '{'}
@@ -9,3 +12,24 @@ def isValid(s: str) -> bool:
         else:
             return False
     return not stack
+
+
+class MinStack:
+
+    def __init__(self):
+        self.data: List[Tuple[int, int]] = []
+
+    def push(self, val: int) -> None:
+        if not self.data:
+            self.data.append((val, val))
+        else:
+            self.data.append((val, min(val, self.getMin())))
+
+    def pop(self) -> None:
+        self.data.pop()
+
+    def top(self) -> int:
+        return self.data[-1][0]
+
+    def getMin(self) -> int:
+        return self.data[-1][1]

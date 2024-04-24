161e38343262825cac294c9cbd2f1dacf3b68019
kareemadesola <kareemadesola1999@gmail.com>
2024-04-24 11:37:23 +0100

    74. Search a 2D Matrix
    Signed-off-by: kareemadesola <kareemadesola1999@gmail.com>

--- neetcode/binary_search.py
+++ neetcode/binary_search.py
@@ -12,3 +12,27 @@ def search(nums: List[int], target: int) -> int:
         else:
             return mid
     return -1
+
+
+def searchMatrix(matrix: List[List[int]], target: int) -> bool:
+    l, r = 0, len(matrix)
+    while l < r:
+        mid = l + (r - l) // 2
+        if matrix[mid][-1] < target:
+            l = mid + 1
+        else:
+            r = mid
+    if l == len(matrix):
+        return False
+
+    row_idx = l
+    l, r = 0, len(matrix[0])
+    while l <= r:
+        mid = l + (r - l) // 2
+        if matrix[row_idx][mid] < target:
+            l = mid + 1
+        elif matrix[row_idx][mid] > target:
+            r = mid - 1
+        else:
+            return True
+    return False
